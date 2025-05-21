ddce988d2645c2722afea71e7764be0efa306cfd
Zelalem Moges <mogeszelalem777@gmail.com>
2025-05-21 13:28:45 +0300

    making the view section

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:index, :show]
+
 end
