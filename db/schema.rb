d54995e036383790f5fd54aaf3550496a9c45845
Zelalem Moges <mogeszelalem777@gmail.com>
2025-05-30 14:34:14 +0300

    first commit for the users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_29_194739) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_30_111850) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,9 +18,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_29_194739) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
